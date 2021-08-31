4da1e24021ea53cc75ce4970569c79ce19ebb6ad
dirkarnez <smalldirkalex@gmail.com>
2021-08-31 16:02:47 +0800

    - add fake DB

--- main.go
+++ main.go
@@ -4,14 +4,39 @@ import (
 	"fmt"
 )
 
+type DB struct {
+	IntData []int
+	StringData []string
+}
+
 // unexport
-func print[T any](s []T) {
-	for _, v := range s {
-		fmt.Print(v)
+func where[T any](a []T, predicate func (t T) bool) {
+	for _, v := range a {
+		if predicate(v) == true{
+			fmt.Print(v)
+		}
 	}
 }
 
 func main() {
-	print([]string{"Hello, ", "playground\n"})
-	print([]int{1, 2, 3})
+	db := DB{
+		IntData: []int{2, 3, 5, 7, 11, 13},
+		StringData: []string{"Hello", "playground"},
+	}
+
+	where(db.StringData, func (str string) bool {
+		if len(str) > 5 {
+			return true
+		} else {
+			return false
+		}
+	})
+
+	where(db.IntData, func (value int) bool {
+		if value > 5 {
+			return true
+		} else {
+			return false
+		}
+	})
 }
